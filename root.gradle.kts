86aba589329998229ba2015db8339d3140fb8aab
sychic <47618543+Sychic@users.noreply.github.com>
2024-01-08 21:09:58 -0500

    Version: beta.11

--- root.gradle.kts
+++ root.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     id("gg.essential.multi-version.root")
 }
 
+version = "beta.11"
+
 // Loom tries to find the active mixin version by recursing up to the root project and checking each project's
 // compileClasspath and build script classpath (in that order). Since we've loom in our root project's classpath,
 // loom will only find it after checking the root project's compileClasspath (which doesn't exist by default).
