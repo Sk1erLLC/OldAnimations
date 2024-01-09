a4172cd747c2642071ddb068d7a384c2fe47fa8c
sychic <47618543+Sychic@users.noreply.github.com>
2024-01-08 21:08:06 -0500

    OldAnimationsCommand: Add `oam` alias

--- src/main/java/club/sk1er/oldanimations/command/OldAnimationsCommand.java
+++ src/main/java/club/sk1er/oldanimations/command/OldAnimationsCommand.java
@@ -4,12 +4,23 @@ import club.sk1er.oldanimations.OldAnimations;
 import gg.essential.api.commands.Command;
 import gg.essential.api.commands.DefaultHandler;
 import gg.essential.api.utils.GuiUtil;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public class OldAnimationsCommand extends Command {
     public OldAnimationsCommand() {
         super("oldanimations");
+    }
+
+    @Nullable
+    @Override
+    public Set<Alias> getCommandAliases() {
+        Set<Alias> aliases = new HashSet<>();
+        aliases.add(new Alias("oam", false));
+        return aliases;
     }
 
     @DefaultHandler
